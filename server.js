2b16273c80124468d08bb6b835fb00d423129f41
himanshu-antal <himanshuantal26@gmail.com>
2026-02-05 10:29:26 +0530

    console

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app.js');
+const chalk = require('chalk');
 const PORT = 3000 || process.env.PORT;
+console.log(chalk.green("Ruuningggg"));
 app.listen(PORT,()=>{
     console.log(`https://localhost:${PORT}`);
 })
